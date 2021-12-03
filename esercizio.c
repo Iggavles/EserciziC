e444a2235c25fcf272a72e0a2943799fbb572100
Iggavles <94383167+Iggavles@users.noreply.github.com>
2021-12-03 15:17:03 +0100

    Add files via upload

---
+++ esercizio.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    if(argc<2)
+    {
+        printf("Non ci sono abbastanza parametri, controlla di aver messo il nome del file\n");
+        exit(-1);
+    }
+    if(argc<3)
+    {
+        printf("Non ci sono abbastanza parametri, controlla di aver messo il numero massimo di caratteri per riga\n");
+        exit(-2);
+    }
+    char riga[argv[2]];
+    File *file;
+    file=fopen(argv[1],"r");
+    if(file==NULL)
+    {
+        printf("il file %s non esiste nella posizione indicata\n",argv[1]);
+        exit(-3);
+    }
+    int x=0;
+    while(fgets(riga,sizeof(riga),file)!=NULL)
+    {
+        printf("%s\n",riga);
+        x=x+1;
+    }
+    fclose(file);
+    return x;
+}
